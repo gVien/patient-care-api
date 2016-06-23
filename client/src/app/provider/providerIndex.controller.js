87e35960276cabdaeed7160f928b8e848cf81488
gVien <wavetech86@gmail.com>
2016-06-22 18:21:41 -0700

    add sweetAlert, use sweetalert instead of toastr for delete

--- client/src/app/provider/providerIndex.controller.js
+++ client/src/app/provider/providerIndex.controller.js
@@ -6,7 +6,7 @@
     .controller('ProvidersIndexController', ProvidersIndexController);
 
   /** @ngInject */
-  function ProvidersIndexController(Provider, $uibModal, $state, $scope, $window, toastr) {
+  function ProvidersIndexController(Provider, $uibModal, $state, $scope, $window, toastr, SweetAlert) {
     var vm = this;
     vm.providers = Provider.query();
 
@@ -21,12 +21,27 @@
 
     // DELETE /api/v1/providers/:id
     $scope.deleteProvider = function(provider) {
-      if (confirm(provider.name + " provider will be deleted. Confirm?")) {
-        provider.$delete(function() {
-          vm.providers = Provider.query();  // reload data
-          toastr.success(provider.name + " provider has been deleted!", "Deleted Successfully:");
-        });
-      }
-  }
+
+      SweetAlert.swal({
+       title: "Are you sure you want to delete " + provider.name +"?",
+       text: "You will not be able to recover this provider!",
+       type: "warning",
+       showCancelButton: true,
+       confirmButtonColor: "#DD6B55",confirmButtonText: "Yes, delete it!",
+       cancelButtonText: "No, cancel it!",
+       closeOnConfirm: false,
+       closeOnCancel: true },
+       function(isConfirm){
+         if (isConfirm) {
+          provider.$delete(function() {
+            vm.providers = Provider.query();  // reload data
+            SweetAlert.swal("Deleted!", "The provider has been deleted!", "success");
+            // toastr.success(provider.name + " provider has been deleted!", "Deleted Successfully:");
+          });
+        } else {
+          SweetAlert.swal("Cancelled", "Your provider is not deleted!", "error");
+        }
+      });
+    }
   }
 })();
